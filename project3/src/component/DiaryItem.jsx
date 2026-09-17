60f02fa378da66aa5fded47f89b443c48509dfc3
rhkrqkq <rhkrqkq1025@gmail.com>
2026-09-17 14:06:24 +0900

    091701 project3

--- project3/src/component/DiaryItem.jsx
+++ project3/src/component/DiaryItem.jsx
@@ -1,11 +1,16 @@
 import "./DiaryItem.css";
 import { useNavigate } from "react-router-dom";
 import { getEmotionImgById } from "../util";
+import Button from "./Button";
 
 const DiaryItem = ({ id, emotionId, content, date }) => {
 	const navigate = useNavigate();
 	const goDetail = () => {
 		navigate(`/diary/${id}`);
+	};
+
+	const goEdit = () => {
+		navigate(`/edit/${id}`);
 	};
 
 	return (
@@ -24,6 +29,9 @@ const DiaryItem = ({ id, emotionId, content, date }) => {
 					{new Date(parseInt(date)).toLocaleDateString()}
 				</div>
 				<div className="content_wrapper">{content.slice(0, 25)}</div>
+			</div>
+			<div className="button_section">
+				<Button onClick={goEdit} text={"수정하기"} />
 			</div>
 		</div>
 	);
--- project3/src/pages/Diary.jsx
+++ project3/src/pages/Diary.jsx
@@ -1,10 +1,43 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import Button from "../component/Button";
+import Header from "../component/Header";
+import { getFormattedDate } from "../util";
+import Viewer from "../component/Viewer";
 
 const Diary = () => {
 	const { id } = useParams();
-	console.log(id);
+	const data = useDiary(id);
+	console.log(data);
 
-	return <div>Diary 페이지입니다.</div>;
+	const navigate = useNavigate();
+
+	const goBack = () => {
+		navigate(-1);
+	};
+
+	const goEdit = () => {
+		navigate(`/edit/${id}`);
+	};
+
+	if (!data) {
+		return <div>일기를 불러오고 있습니다...</div>;
+	} else {
+		const { date, emotionId, content } = data;
+		const title = `${getFormattedDate(new Date(Number(date)))} 기록`;
+		return (
+			<div>
+				<Header
+					title={title}
+					leftChild={<Button text={"뒤로 가기"} onClick={goBack} />}
+					rightChild={<Button text={"수정하기"} onClick={goEdit} />}
+				/>
+				<div>{id}번 일기</div>
+				<div>Diary 페이지입니다</div>
+				<Viewer conent={content} emotionId={emotionId} />
+			</div>
+		);
+	}
 };
 
 export default Diary;
